23a8f0bdabfc0e072bd658764b9cabe980a90163
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-07-14 10:14:53 -0700

    Added the popup information back in.

--- ininital-shiny-app/app.R
+++ ininital-shiny-app/app.R
@@ -24,9 +24,9 @@ library(leaflet)
 SNAPP_estuary_points <- read_sf(dsn = here("locations"), layer = "FINAL_SNAPP_ESTUARIES_POLYGONS-66") %>%
   st_transform(crs = 3310) %>%
   st_centroid(geometry) %>%
-  mutate("Ecology" = Ecol1, "Restoration" = Restor1, "Harvest" = Harvest1, "Community" = Comm1) %>%
+  mutate("Ecology" = Ecol1, "Restoration" = Restor1, "Harvest" = Harvest1, "Community" = Comm1, "Ecology2" = Ecol1, "Restoration2" = Restor1, "Harvest2" = Harvest1, "Community2" = Comm1) %>%
   select(-NCEAM) %>%
-  gather(score_type, score, -Estuary_Na, -geometry, -Ecology, -Restoration, -Harvest, -Community) %>%
+  gather(score_type, score, -Estuary_Na, -geometry, -Ecology, -Restoration, -Harvest, -Community, -Ecology2, -Restoration2, -Harvest2, -Community2) %>%
   mutate(score_type1 = score_type)
 
 basemap_streets <- tm_basemap("Esri.WorldStreetMap")
@@ -46,7 +46,7 @@ ui <- fluidPage(
                  
                  selectInput(inputId = "slider_select",
                              label = "select slider score",
-                             choices = c("Ecology", "Restoration", "Harvest", "Community")
+                             choices = c(Ecology = "Ecology2", Restoration = "Restoration2", Harvest = "Harvest2", "Community" = "Community2")
                              ),
                  
                  
@@ -70,7 +70,7 @@ server <- function(inputs, outputs) {
   estuary_shiny <- reactive({
     SNAPP_estuary_points %>%
       filter(score_type %in% (inputs$aqua_score)) %>%
-     select(inputs$slider_select, Estuary_Na, score ) %>%
+     #select(inputs$slider_select, Estuary_Na, score, Ecology, Restoration, Harvest, Community ) %>%
     filter(score >= inputs$aqua_score_range[1] & score <= inputs$aqua_score_range[2]) 
   })
   
@@ -86,7 +86,7 @@ server <- function(inputs, outputs) {
         size = 0.25, 
         palette = "Purples", 
         title = "Score", 
-        #popup.vars = c("score", "Ecology", "Restoration", "Harvest", "Community")
+        popup.vars = c("Ecology", "Restoration", "Harvest", "Community")
         ) +
       basemap_streets
     tmap_leaflet(SNAPP_estuary_map_points)
